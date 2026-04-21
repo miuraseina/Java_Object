aac51549949af3f2a3c6e00ed0d927f31f40f559
miuraseina <s.miura@miracleave.co.jp>
2026-04-21 15:39:20 +0900

    first commit

---
+++ J2-Z1_統合開発環境（eclipse）の利用/サンプルソースコード/Sample2_Z1_1s.java
@@ -0,0 +1,7 @@
+class Sample2_Z1_1s {
+	public static void main (String[] args) {
+		
+		System.out.println("これは文字コードがSJISのコードです");
+		
+	}
+}
---
+++ J2-Z1_統合開発環境（eclipse）の利用/サンプルソースコード（UTF-8）/Sample2_Z1_2.java
@@ -0,0 +1,9 @@
+class Sample2_Z1_2 {
+	public static void main (String[] args) {
+		
+		System.out.println("コマンドライン引数1：" + args[0] );
+		
+		System.out.println("コマンドライン引数2：" + args[1] );
+		
+	}
+}
